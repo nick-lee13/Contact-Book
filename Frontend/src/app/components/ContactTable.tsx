74690f2533f04bbf69cfd9efdac939c3f7b8715f
nick-lee13 <ntlee@mta.ca>
2023-09-08 10:45:29 -0700

    Front end updated to use API
    Fronted now uses backend API through endpoints instead of using local memory

--- Frontend/src/app/components/ContactTable.tsx
+++ Frontend/src/app/components/ContactTable.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 import ContactRow from "./ContactRow";
 import ContactForm from "./ContactForm";
+import axios from "axios"; // Import Axios
 
 const ContactTable = () => {
   const [contactData, setContactData] = useState<Contact[]>([]);
@@ -12,35 +13,71 @@ const ContactTable = () => {
     fetchUserData();
   }, []);
 
+  const API_URL="http://localhost:5038/";
+
   const fetchUserData = async () => {
-    //await fetch api
+    /*//await fetch api
     let res = localStorage.getItem("@contact-data");
     let json = [];
     if (res != null) json = JSON.parse(res);
-    setContactData(json);
+    setContactData(json);*/
+
+    // Updated to use API requests
+    try{
+      const response = await axios.get(API_URL+"api/contacts/GetContacts");
+    setContactData(response.data);
+    } catch(error){
+      console.error("Error fetching contact data from database: ", error);
+    }
   };
 
   const deleteUserData = async (id: string) => {
-    //await delete api
+    /*//await delete api
     let temp = contactData.filter((el) => el.id !== id);
     localStorage.setItem("@contact-data", JSON.stringify(temp));
-    fetchUserData()
+    fetchUserData()*/
+
+    // API Request to delete contact data
+    try{
+      await axios.delete(`${API_URL}api/contacts/DeleteContacts?id=${id}`);
+      fetchUserData();
+    } catch(error){
+      console.error("Error deleting contact: ", error);
+    }
   };
 
   const updateUserData = async (contact: Contact) => {
     if (selectedIndex != -1) {
-      //await update api
+      /*//await update api
       let temp = contactData;
       temp[selectedIndex] = contact;
-      localStorage.setItem("@contact-data", JSON.stringify(temp));
+      localStorage.setItem("@contact-data", JSON.stringify(temp));*/
+
+      // API request to update a contact
+      try{
+        console.log("ID to update: ", contact.id);
+        await axios.patch(`${API_URL}api/contacts/UpdateContacts?id=${contact.id}`, contact);
+        fetchUserData();
+        closeFormModal();
+      } catch(error){
+        console.error("Error updating contact: ", error);
+      }
+      
     } else {
-      //await add api
+      /*//await add api
       let temp = contactData;
       temp.push(contact);
-      localStorage.setItem("@contact-data", JSON.stringify(temp));
+      localStorage.setItem("@contact-data", JSON.stringify(temp));*/
+
+      // API request to add a contact
+      try{
+        await axios.post(API_URL+"api/contacts/AddContacts", contact);
+        fetchUserData();
+        closeFormModal();
+      } catch(error){
+        console.error("Error adding contact: ", error);
+      }
     }
-    fetchUserData()
-    closeFormModal();
   };
 
   const closeFormModal = () => {
